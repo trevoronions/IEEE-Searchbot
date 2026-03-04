f93a645a7e33b3818622851cefddc86846b552f2
Trevor Onions <trevoronions@u.boisestate.edu>
2026-03-04 13:03:11 -0700

    Created an implementation file that only has functions we need

--- BFSalgo/implementation.cpp
+++ BFSalgo/implementation.cpp
@@ -1,11 +1,3 @@
-/*
- Sample code from https://www.redblobgames.com/pathfinding/a-star/
- Copyright 2014 Red Blob Games <redblobgames@gmail.com>
- 
- Feel free to use this code in your own projects, including commercial projects
- License: Apache v2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
-*/
-
 #include <iostream>
 #include <iomanip>
 #include <unordered_map>
@@ -19,26 +11,28 @@
 #include <cstdlib>
 using namespace std; 
 
-struct SimpleGraph {
-  std::unordered_map<char, std::vector<char> > edges;
-
-  std::vector<char> neighbors(char id) {
-    return edges[id];
-  }
-};
-
-SimpleGraph example_graph {{
-    {'A', {'B'}},
-    {'B', {'C'}},
-    {'C', {'B', 'D', 'F'}},
-    {'D', {'C', 'E'}},
-    {'E', {'F'}},
-    {'F', {}},
-  }};
-
 struct GridLocation {
   int x, y;
 };
+
+// Helpers for GridLocation
+
+bool operator == (GridLocation a, GridLocation b) {
+  return a.x == b.x && a.y == b.y;
+}
+
+bool operator != (GridLocation a, GridLocation b) {
+  return !(a == b);
+}
+
+bool operator < (GridLocation a, GridLocation b) {
+  return std::tie(a.x, a.y) < std::tie(b.x, b.y);
+}
+
+std::basic_iostream<char>::basic_ostream& operator<<(std::basic_iostream<char>::basic_ostream& out, const GridLocation& loc) {
+  out << '(' << loc.x << ',' << loc.y << ')';
+  return out;
+}
 
 namespace std {
 /* implement hash function so we can put GridLocation into an unordered_set */
@@ -93,30 +87,73 @@ std::array<GridLocation, 4> SquareGrid::DIRS = {
   GridLocation{1, 0}, GridLocation{-1, 0},
   GridLocation{0, -1}, GridLocation{0, 1}
 };
+template<typename Location, typename Graph>
+std::unordered_map<Location, Location>
+breadth_first_search(Graph graph, Location start) {
+  std::queue<Location> frontier;
+  frontier.push(start);
 
-// Helpers for GridLocation
+  std::unordered_map<Location, Location> came_from;
+  came_from[start] = start;
 
-bool operator == (GridLocation a, GridLocation b) {
-  return a.x == b.x && a.y == b.y;
+  while (!frontier.empty()) {
+    Location current = frontier.front();
+    frontier.pop();
+
+    for (Location next : graph.neighbors(current)) {
+      if (came_from.find(next) == came_from.end()) {
+        frontier.push(next);
+        came_from[next] = current;
+      }
+    }
+  }
+  return came_from;
 }
 
-bool operator != (GridLocation a, GridLocation b) {
-  return !(a == b);
+template<typename Location, typename Graph>
+unordered_map<Location, Location>
+breadth_first_search(Graph graph, Location start, Location goal) {
+  queue<Location> frontier; //defining frontier as a queue data struct
+  frontier.push(start);
+
+  unordered_map<Location, Location> came_from;
+  came_from[start] = start;
+
+  while (!frontier.empty()) {
+    Location current = frontier.front(); //assigning top of frontier to current
+    frontier.pop(); //removes first element
+
+
+    if (current == goal) {
+      break;
+    }
+    
+    for (Location next : graph.neighbors(current)) {
+      if (came_from.find(next) == came_from.end()) {
+        frontier.push(next);
+        came_from[next] = current;
+      }
+    }
+  }
+  return came_from;
+}
+void add_rect(SquareGrid& grid, int x1, int y1, int x2, int y2) {
+  for (int x = x1; x < x2; ++x) {
+    for (int y = y1; y < y2; ++y) {
+      grid.walls.insert(GridLocation{x, y});
+    }
+  }
 }
 
-bool operator < (GridLocation a, GridLocation b) {
-  return std::tie(a.x, a.y) < std::tie(b.x, b.y);
+SquareGrid make_diagram1() {
+  SquareGrid grid(30, 15);
+  add_rect(grid, 3, 3, 5, 12);
+  add_rect(grid, 13, 4, 15, 15);
+  add_rect(grid, 21, 0, 23, 7);
+  add_rect(grid, 23, 5, 26, 7);
+  return grid;
 }
 
-std::basic_iostream<char>::basic_ostream& operator<<(std::basic_iostream<char>::basic_ostream& out, const GridLocation& loc) {
-  out << '(' << loc.x << ',' << loc.y << ')';
-  return out;
-}
-
-// This outputs a grid. Pass in a distances map if you want to print
-// the distances, or pass in a point_to map if you want to print
-// arrows that point to the parent location, or pass in a path vector
-// if you want to draw the path.
 template<class Graph>
 void draw_grid(const Graph& graph,
                std::unordered_map<GridLocation, double>* distances=nullptr,
@@ -155,100 +192,6 @@ void draw_grid(const Graph& graph,
   std::cout << std::string(field_width * graph.width, '~') << '\n';
 }
 
-void add_rect(SquareGrid& grid, int x1, int y1, int x2, int y2) {
-  for (int x = x1; x < x2; ++x) {
-    for (int y = y1; y < y2; ++y) {
-      grid.walls.insert(GridLocation{x, y});
-    }
-  }
-}
-
-SquareGrid make_diagram1() {
-  SquareGrid grid(30, 15);
-  add_rect(grid, 3, 3, 5, 12);
-  add_rect(grid, 13, 4, 15, 15);
-  add_rect(grid, 21, 0, 23, 7);
-  add_rect(grid, 23, 5, 26, 7);
-  return grid;
-}
-
-struct GridWithWeights: SquareGrid {
-  std::unordered_set<GridLocation> forests;
-  GridWithWeights(int w, int h): SquareGrid(w, h) {}
-  double cost(GridLocation from_node, GridLocation to_node) const {
-    return forests.find(to_node) != forests.end()? 5 : 1;
-  }
-};
-
-GridWithWeights make_diagram4() {
-  GridWithWeights grid(10, 10);
-  add_rect(grid, 1, 7, 4, 9);
-  typedef GridLocation L;
-  grid.forests = std::unordered_set<GridLocation> {
-    L{3, 4}, L{3, 5}, L{4, 1}, L{4, 2},
-    L{4, 3}, L{4, 4}, L{4, 5}, L{4, 6},
-    L{4, 7}, L{4, 8}, L{5, 1}, L{5, 2},
-    L{5, 3}, L{5, 4}, L{5, 5}, L{5, 6},
-    L{5, 7}, L{5, 8}, L{6, 2}, L{6, 3},
-    L{6, 4}, L{6, 5}, L{6, 6}, L{6, 7},
-    L{7, 3}, L{7, 4}, L{7, 5}
-  };
-  return grid;
-}
-
-template<typename T, typename priority_t>
-struct PriorityQueue {
-  typedef std::pair<priority_t, T> PQElement;
-  std::priority_queue<PQElement, std::vector<PQElement>,
-                 std::greater<PQElement>> elements;
-
-  inline bool empty() const {
-     return elements.empty();
-  }
-
-  inline void put(T item, priority_t priority) {
-    elements.emplace(priority, item);
-  }
-
-  T get() {
-    T best_item = elements.top().second;
-    elements.pop();
-    return best_item;
-  }
-};
-
-template<typename Location, typename Graph>
-void dijkstra_search
-  (Graph graph,
-   Location start,
-   Location goal,
-   std::unordered_map<Location, Location>& came_from,
-   std::unordered_map<Location, double>& cost_so_far)
-{
-  PriorityQueue<Location, double> frontier;
-  frontier.put(start, 0);
-
-  came_from[start] = start;
-  cost_so_far[start] = 0;
-  
-  while (!frontier.empty()) {
-    Location current = frontier.get();
-
-    if (current == goal) {
-      break;
-    }
-
-    for (Location next : graph.neighbors(current)) {
-      double new_cost = cost_so_far[current] + graph.cost(current, next);
-      if (cost_so_far.find(next) == cost_so_far.end()
-          || new_cost < cost_so_far[next]) {
-        cost_so_far[next] = new_cost;
-        came_from[next] = current;
-        frontier.put(next, new_cost);
-      }
-    }
-  }
-}
 
 template<typename Location>
 std::vector<Location> reconstruct_path(
@@ -267,76 +210,4 @@ std::vector<Location> reconstruct_path(
   path.push_back(start); // optional
   std::reverse(path.begin(), path.end());
   return path;
-}
-
-GridWithWeights make_diagram_nopath() {
-  GridWithWeights grid(10, 10);
-  add_rect(grid, 5, 0, 6, 10);
-  return grid;
-}
-
-inline double heuristic(GridLocation a, GridLocation b) {
-  return std::abs(a.x - b.x) + std::abs(a.y - b.y);
-}
-
-template<typename Location, typename Graph>
-void a_star_search
-  (Graph graph,
-   Location start,
-   Location goal,
-   std::unordered_map<Location, Location>& came_from,
-   std::unordered_map<Location, double>& cost_so_far)
-{
-  PriorityQueue<Location, double> frontier;
-  frontier.put(start, 0);
-
-  came_from[start] = start;
-  cost_so_far[start] = 0;
-  
-  while (!frontier.empty()) {
-    Location current = frontier.get();
-
-    if (current == goal) {
-      break;
-    }
-
-    for (Location next : graph.neighbors(current)) {
-      double new_cost = cost_so_far[current] + graph.cost(current, next);
-      if (cost_so_far.find(next) == cost_so_far.end()
-          || new_cost < cost_so_far[next]) {
-        cost_so_far[next] = new_cost;
-        double priority = new_cost + heuristic(next, goal);
-        frontier.put(next, priority);
-        came_from[next] = current;
-      }
-    }
-  }
-}
-
-template<typename Location, typename Graph>
-unordered_map<Location, Location>
-breadth_first_search(Graph graph, Location start, Location goal) {
-  queue<Location> frontier; //defining frontier as a queue data struct
-  frontier.push(start);
-
-  unordered_map<Location, Location> came_from;
-  came_from[start] = start;
-
-  while (!frontier.empty()) {
-    Location current = frontier.front(); //assigning top of frontier to current
-    frontier.pop(); //removes first element
-
-
-    if (current == goal) {
-      break;
-    }
-    
-    for (Location next : graph.neighbors(current)) {
-      if (came_from.find(next) == came_from.end()) {
-        frontier.push(next);
-        came_from[next] = current;
-      }
-    }
-  }
-  return came_from;
 }
